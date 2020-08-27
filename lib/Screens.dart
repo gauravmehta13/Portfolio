243e394d5cd192c2f26ad6a07e293a4bb38c9930
gaurav <270mehta@gmail.com>
2020-08-27 18:13:52 +0530

    Added Bottom Icons

--- lib/Screens.dart
+++ lib/Screens.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 class HomePage extends StatefulWidget {
@@ -12,7 +13,7 @@ class _HomePageState extends State<HomePage> {
     return Scaffold(
       body: Container(
         width: double.infinity,
-        padding: EdgeInsets.fromLTRB(50, 30, 50, 0),
+        padding: EdgeInsets.fromLTRB(50, 30, 50, 20),
         decoration: BoxDecoration(
             color: Colors.black, //Color(0xFF141414),
             image: DecorationImage(
@@ -41,7 +42,7 @@ class _HomePageState extends State<HomePage> {
                       ),
                     ),
                     SizedBox(
-                      width: 30,
+                      width: 20,
                     ),
                     FlatButton(
                       onPressed: () {},
@@ -51,18 +52,24 @@ class _HomePageState extends State<HomePage> {
                       ),
                     ),
                     SizedBox(
-                      width: 30,
+                      width: 20,
                     ),
-                    Text(
-                      'Projects',
-                      style: TextStyle(fontWeight: FontWeight.w600),
+                    FlatButton(
+                      onPressed: () {},
+                      child: Text(
+                        'Projects',
+                        style: TextStyle(fontWeight: FontWeight.w600),
+                      ),
                     ),
                     SizedBox(
-                      width: 30,
+                      width: 20,
                     ),
-                    Text(
-                      'Contact Me',
-                      style: TextStyle(fontWeight: FontWeight.w600),
+                    FlatButton(
+                      onPressed: () {},
+                      child: Text(
+                        'Contact Me',
+                        style: TextStyle(fontWeight: FontWeight.w600),
+                      ),
                     )
                   ],
                 )
@@ -87,6 +94,33 @@ class _HomePageState extends State<HomePage> {
                             fontSize: 100,
                             fontWeight: FontWeight.w900),
                       )),
+                ],
+              ),
+            ),
+            Align(
+              alignment: Alignment.bottomLeft,
+              child: Row(
+                children: <Widget>[
+                  IconButton(
+                      icon: FaIcon(FontAwesomeIcons.instagram),
+                      onPressed: () {
+                        print("Pressed");
+                      }),
+                  IconButton(
+                      icon: FaIcon(FontAwesomeIcons.medium),
+                      onPressed: () {
+                        print("Pressed");
+                      }),
+                  IconButton(
+                      icon: FaIcon(FontAwesomeIcons.github),
+                      onPressed: () {
+                        print("Pressed");
+                      }),
+                  IconButton(
+                      icon: FaIcon(FontAwesomeIcons.linkedin),
+                      onPressed: () {
+                        print("Pressed");
+                      })
                 ],
               ),
             )
