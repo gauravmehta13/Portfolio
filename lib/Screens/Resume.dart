39510009e703c77aae4e7d25af43000b4b301eea
gaurav <270mehta@gmail.com>
2020-08-28 19:51:01 +0530

    Added Resume

--- lib/Screens/Resume.dart
+++ lib/Screens/Resume.dart
@@ -3,6 +3,15 @@ import 'package:flutter/material.dart';
 class Resume extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Container(
+        child: Column(
+          children: <Widget>[
+            FlatButton(onPressed: null, child: null),
+            // Image.network()
+          ],
+        ),
+      ),
+    );
   }
 }
