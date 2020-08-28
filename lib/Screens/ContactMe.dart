c58d3a1771fd352243d40a0dc0c425423df388b7
gaurav <270mehta@gmail.com>
2020-08-28 17:26:42 +0530

    Minor Fixes

--- lib/Screens/ContactMe.dart
+++ lib/Screens/ContactMe.dart
@@ -22,188 +22,182 @@ class ContactMe extends StatelessWidget {
       ),
       body: SingleChildScrollView(
         child: Container(
-          padding: EdgeInsets.fromLTRB(10, 0, 10, 0),
-          color: Color(0xFF141414),
+          decoration: BoxDecoration(
+              /*image: DecorationImage(
+                  image: NetworkImage(
+                      'https://raw.githubusercontent.com/gauravmehta13/Portfolio/master/IMG_20200827_225429.jpg'),
+                  fit: BoxFit.fitWidth)*/
+              ),
+          padding: EdgeInsets.fromLTRB(50, 20, 10, 0),
+          //  color: Color(0xFF141414),
           width: MediaQuery.of(context).size.width,
-          height: MediaQuery.of(context).size.height * 0.7,
-          child: Row(
-            children: [
-              Spacer(),
-              Container(
-                // color: Colors.transparent,
-                //  width: MediaQuery.of(context).size.width / 3,
-                child: Column(
-                  // mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    Text('Contact Me',
-                        style: GoogleFonts.montserrat(
-                          textStyle: TextStyle(
-                              color: Colors.white,
-                              fontSize: 60,
-                              fontWeight: FontWeight.w400),
-                        )),
-                    SizedBox(
-                      height: 15,
+          // height: MediaQuery.of(context).size.height * 0.7,
+          child: Container(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text('Contact Me',
+                    style: GoogleFonts.montserrat(
+                      textStyle: TextStyle(
+                          color: Colors.white,
+                          fontSize: 60,
+                          fontWeight: FontWeight.w400),
+                    )),
+                SizedBox(
+                  height: 15,
+                ),
+                Text(
+                    'Do you speak Japnese? Its ok if you dont\nI speak English too.',
+                    style: GoogleFonts.montserrat(
+                      textStyle: TextStyle(
+                          color: Colors.grey[700],
+                          fontSize: 20,
+                          fontWeight: FontWeight.w400),
+                    )),
+                SizedBox(
+                  height: 15,
+                ),
+                FlatButton(
+                  onPressed: () {
+                    html.window.open(
+                        'https://www.linkedin.com/in/gauravmehta13/',
+                        'new tab');
+                  },
+                  child: Row(
+                    children: <Widget>[
+                      IconButton(
+                        icon: FaIcon(
+                          FontAwesomeIcons.linkedin,
+                          color: Color(0xFF0072b1),
+                        ),
+                        onPressed: null,
+                      ),
+                      Text(
+                        'Connect With Me on LinkedIn',
+                        style: TextStyle(
+                          color: Color(0xFF0072b1),
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+                FlatButton(
+                  onPressed: () {
+                    html.window.open('https://wa.me/+917073142922', 'new tab');
+                  },
+                  child: Row(
+                    children: <Widget>[
+                      IconButton(
+                        icon: FaIcon(
+                          FontAwesomeIcons.whatsapp,
+                          color: Colors.green,
+                        ),
+                        onPressed: null,
+                      ),
+                      Text(
+                        'Message Me on Whatsapp',
+                        style: TextStyle(color: Colors.green),
+                      )
+                    ],
+                  ),
+                ),
+                FlatButton(
+                  onPressed: () {
+                    html.window
+                        .open('https://github.com/gauravmehta13', 'new tab');
+                  },
+                  child: Row(
+                    children: <Widget>[
+                      IconButton(
+                        icon: FaIcon(
+                          FontAwesomeIcons.github,
+                          color: Colors.white60,
+                        ),
+                        onPressed: null,
+                      ),
+                      Text(
+                        'Follow Me on Github',
+                        style: TextStyle(
+                          color: Colors.white60,
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+                FlatButton(
+                  onPressed: () {
+                    html.window.open('https://t.me/gauravmehta13', 'new tab');
+                  },
+                  child: Row(
+                    children: <Widget>[
+                      IconButton(
+                        icon: FaIcon(
+                          FontAwesomeIcons.telegram,
+                          color: Color(0xFF0088cc),
+                        ),
+                        onPressed: null,
+                      ),
+                      Text(
+                        'Message Me on Telegram',
+                        style: TextStyle(
+                          color: Color(0xFF0088cc),
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: <Widget>[
+                    IconButton(
+                      icon: FaIcon(
+                        FontAwesomeIcons.instagram,
+                        color: Color(0xFFDD2a7b),
+                      ),
+                      onPressed: () {
+                        html.window.open(
+                            'https://github.com/gauravmehta13', 'new tab');
+                      },
                     ),
-                    Text(
-                        'Do you speak Japnese? Its ok if you dont\nI speak English too.',
-                        style: GoogleFonts.montserrat(
-                          textStyle: TextStyle(
-                              color: Colors.grey[700],
-                              fontSize: 20,
-                              fontWeight: FontWeight.w400),
-                        )),
-                    SizedBox(
-                      height: 15,
+                    IconButton(
+                      icon: FaIcon(
+                        FontAwesomeIcons.twitter,
+                        color: Color(0xFF00acee),
+                      ),
+                      onPressed: () {
+                        html.window.open(
+                            'https://twitter.com/gauravmehta_', 'new tab');
+                      },
                     ),
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        FlatButton(
-                          onPressed: () {
-                            html.window.open(
-                                'https://www.linkedin.com/in/gauravmehta13/',
-                                'new tab');
-                          },
-                          child: Row(
-                            children: <Widget>[
-                              IconButton(
-                                icon: FaIcon(
-                                  FontAwesomeIcons.linkedin,
-                                  color: Color(0xFF0072b1),
-                                ),
-                                onPressed: null,
-                              ),
-                              Text(
-                                'Connect With Me on LinkedIn',
-                                style: TextStyle(
-                                  color: Color(0xFF0072b1),
-                                ),
-                              )
-                            ],
-                          ),
-                        ),
-                        FlatButton(
-                          onPressed: () {
-                            html.window
-                                .open('https://wa.me/+917073142922', 'new tab');
-                          },
-                          child: Row(
-                            children: <Widget>[
-                              IconButton(
-                                icon: FaIcon(
-                                  FontAwesomeIcons.whatsapp,
-                                  color: Colors.green,
-                                ),
-                                onPressed: null,
-                              ),
-                              Text(
-                                'Message Me on Whatsapp',
-                                style: TextStyle(color: Colors.green),
-                              )
-                            ],
-                          ),
-                        ),
-                        FlatButton(
-                          onPressed: () {
-                            html.window.open(
-                                'https://github.com/gauravmehta13', 'new tab');
-                          },
-                          child: Row(
-                            children: <Widget>[
-                              IconButton(
-                                icon: FaIcon(
-                                  FontAwesomeIcons.github,
-                                  color: Colors.white60,
-                                ),
-                                onPressed: null,
-                              ),
-                              Text(
-                                'Follow Me on Github',
-                                style: TextStyle(
-                                  color: Colors.white60,
-                                ),
-                              )
-                            ],
-                          ),
-                        ),
-                        FlatButton(
-                          onPressed: () {
-                            html.window
-                                .open('https://t.me/gauravmehta13', 'new tab');
-                          },
-                          child: Row(
-                            children: <Widget>[
-                              IconButton(
-                                icon: FaIcon(
-                                  FontAwesomeIcons.telegram,
-                                  color: Color(0xFF0088cc),
-                                ),
-                                onPressed: null,
-                              ),
-                              Text(
-                                'Message Me on Telegram',
-                                style: TextStyle(
-                                  color: Color(0xFF0088cc),
-                                ),
-                              )
-                            ],
-                          ),
-                        ),
-                      ],
+                    IconButton(
+                      icon: Icon(
+                        Icons.mail_outline,
+                      ),
+                      color: Color(0xFFD44638),
+                      onPressed: () {
+                        html.window
+                            .open('mailto:gauravmehta@outlook.in', 'new tab');
+                      },
                     ),
-                    SizedBox(
-                      height: 20,
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: <Widget>[
-                        IconButton(
-                          icon: FaIcon(
-                            FontAwesomeIcons.instagram,
-                            color: Color(0xFFDD2a7b),
-                          ),
-                          onPressed: () {
-                            html.window.open(
-                                'https://github.com/gauravmehta13', 'new tab');
-                          },
-                        ),
-                        IconButton(
-                          icon: FaIcon(
-                            FontAwesomeIcons.twitter,
-                            color: Color(0xFF00acee),
-                          ),
-                          onPressed: () {
-                            html.window.open(
-                                'https://twitter.com/gauravmehta_', 'new tab');
-                          },
-                        ),
-                        IconButton(
-                          icon: Icon(
-                            Icons.mail_outline,
-                          ),
-                          color: Color(0xFFD44638),
-                          onPressed: () {
-                            html.window.open(
-                                'mailto:gauravmehta@outlook.in', 'new tab');
-                          },
-                        ),
-                        IconButton(
-                          icon: FaIcon(
-                            FontAwesomeIcons.medium,
-                            color: Colors.white70,
-                          ),
-                          onPressed: () {
-                            html.window.open(
-                                'https://medium.com/@gauravmehta13', 'new tab');
-                          },
-                        ),
-                      ],
+                    IconButton(
+                      icon: FaIcon(
+                        FontAwesomeIcons.medium,
+                        color: Colors.white70,
+                      ),
+                      onPressed: () {
+                        html.window.open(
+                            'https://medium.com/@gauravmehta13', 'new tab');
+                      },
                     ),
                   ],
                 ),
-              ),
-              Container(
+              ],
+            ),
+          ),
+          /* Container(
                 alignment: Alignment.topCenter,
                 width: MediaQuery.of(context).size.width / 2,
                 decoration: BoxDecoration(
@@ -211,9 +205,7 @@ class ContactMe extends StatelessWidget {
                         image: NetworkImage(
                             'https://raw.githubusercontent.com/gauravmehta13/Portfolio/master/IMG_20200827_225429.jpg'),
                         fit: BoxFit.fitHeight)),
-              ),
-            ],
-          ),
+              ),*/
         ),
       ),
     );
