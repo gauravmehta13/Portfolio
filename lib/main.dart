85d68554f049d0e1418ad68a0fb054679645d807
gaurav <270mehta@gmail.com>
2020-08-28 11:23:18 +0530

    Corrected initial route

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        initialRoute: '/Blog',
+        initialRoute: '/',
         routes: {
           '/': (context) => HomePage(),
           '/Resume': (context) => Resume(),
