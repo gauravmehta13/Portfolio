01618b195a9365cb23b2c35da26a44e47002fe86
gaurav <270mehta@gmail.com>
2020-09-08 17:59:48 +0530

    Minor Fixes

--- lib/Screens/HomePage/MobileHomePage.dart
+++ lib/Screens/HomePage/MobileHomePage.dart
@@ -9,7 +9,6 @@ class MobileHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      drawer: SideBar(),
       body: Container(
         width: MediaQuery.of(context).size.width,
         padding: EdgeInsets.fromLTRB(0, 0, 0, 0),
@@ -21,70 +20,63 @@ class MobileHomePage extends StatelessWidget {
             SingleChildScrollView(
               child: Align(
                 alignment: Alignment.bottomRight,
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.end,
-                  mainAxisSize: MainAxisSize.max,
-                  mainAxisAlignment: MainAxisAlignment.end,
+                child: Stack(
                   children: <Widget>[
                     Image(
                       image: NetworkImage(
                         "https://raw.githubusercontent.com/gauravmehta13/Portfolio/master/Assets/HomeView.jpg",
                       ),
                       fit: BoxFit.fitWidth,
+                    ),
+                    Column(
+                      children: [
+                        SizedBox(
+                          height: MediaQuery.of(context).size.height / 5,
+                        ),
+                        Row(
+                          children: [
+                            SizedBox(width: 80),
+                            Container(
+                              width: MediaQuery.of(context).size.width / 2,
+                              alignment: Alignment.topLeft,
+                              child: Column(
+                                children: [
+                                  Align(
+                                    alignment: Alignment.center,
+                                    child: Text('Hello, I\u0027m\nGaurav Yadav',
+                                        style: GoogleFonts.montserrat(
+                                          textStyle: TextStyle(
+                                              color: Colors.white,
+                                              fontSize: 40,
+                                              fontWeight: FontWeight.w400),
+                                        )),
+                                  ),
+                                  SizedBox(
+                                    height: 30,
+                                  ),
+                                  Text(
+                                      'An Engineering Student and a tech enthusiast working \nto better understand the core concepts behind different \npopular Technologies like Artificial Intelligence,DevOps,\nCloud Computing etc.',
+                                      style: GoogleFonts.montserrat(
+                                        textStyle: TextStyle(
+                                          color: Colors.white54,
+                                        ),
+                                      )),
+                                  SizedBox(
+                                    height: 10,
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
                     )
                   ],
                 ),
               ),
             ),
-            //SideBar(),
-            Column(
-              children: <Widget>[
-                Column(
-                  children: [
-                    // Topbar(),
-                    SizedBox(
-                      height: MediaQuery.of(context).size.height / 15,
-                    ),
-                    Row(
-                      children: [
-                        SizedBox(width: 80),
-                        Container(
-                          width: MediaQuery.of(context).size.width / 2,
-                          alignment: Alignment.topLeft,
-                          child: Column(
-                            children: [
-                              Align(
-                                alignment: Alignment.center,
-                                child: Text('Hello, I\u0027m\nGaurav Yadav',
-                                    style: GoogleFonts.montserrat(
-                                      textStyle: TextStyle(
-                                          color: Colors.white,
-                                          fontSize: 60,
-                                          fontWeight: FontWeight.w400),
-                                    )),
-                              ),
-                              SizedBox(
-                                height: 30,
-                              ),
-                              Text(
-                                  'An Engineering Student and a tech enthusiast working \nto better understand the core concepts behind different \npopular Technologies like Artificial Intelligence,DevOps,\nCloud Computing etc.',
-                                  style: GoogleFonts.montserrat(
-                                    textStyle: TextStyle(
-                                      color: Colors.grey[700],
-                                    ),
-                                  )),
-                              SizedBox(
-                                height: 10,
-                              ),
-                            ],
-                          ),
-                        ),
-                      ],
-                    ),
-                  ],
-                )
-              ],
-            ),
+            Topbar(),
+            SideBar(),
           ],
         ),
       ),
