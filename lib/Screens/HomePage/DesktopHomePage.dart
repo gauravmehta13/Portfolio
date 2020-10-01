c8a65f82bff4e63d1bf1c3c70642fb0d765deaf8
gaurav <270mehta@gmail.com>
2020-10-01 17:37:43 +0530

    Added parallax view

--- lib/Screens/HomePage/DesktopHomePage.dart
+++ lib/Screens/HomePage/DesktopHomePage.dart
@@ -3,36 +3,28 @@ import 'package:google_fonts/google_fonts.dart';
 import '../../TopBar.dart';
 import 'SideBar.dart';
 
-class FutureBuilderWidget extends StatefulWidget {
+class DesktopHomePage extends StatefulWidget {
   @override
-  _FutureBuilderWidgetState createState() => _FutureBuilderWidgetState();
+  _DesktopHomePageState createState() => _DesktopHomePageState();
 }
 
-Future<String> getdatafromfuture() async {
-  return new Future.delayed(const Duration(seconds: 3), () => 'Sample');
-}
-
-class _FutureBuilderWidgetState extends State<FutureBuilderWidget> {
+class _DesktopHomePageState extends State<DesktopHomePage> {
+  double topimg = 1;
+  double toptext = 150;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: FutureBuilder(
-          future: getdatafromfuture(),
-          builder: (context, snapshot) {
-            if (snapshot.data != null) {
-              return DesktopHomePage();
-            }
-            return Center(child: CircularProgressIndicator());
-          }),
-    );
-  }
-}
-
-class DesktopHomePage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(
+        body: NotificationListener(
+      onNotification: (v) {
+        if (v is ScrollUpdateNotification) {
+          //only if scroll update notification is triggered
+          setState(() {
+            toptext -= v.scrollDelta / 1;
+            topimg -= v.scrollDelta / 1.5;
+          });
+        }
+      },
+      child: Container(
         width: MediaQuery.of(context).size.width,
         padding: EdgeInsets.fromLTRB(0, 0, 0, 0),
         decoration: BoxDecoration(
@@ -40,79 +32,67 @@ class DesktopHomePage extends StatelessWidget {
         ),
         child: Stack(
           children: <Widget>[
-            SingleChildScrollView(
-              child: Align(
-                alignment: Alignment.bottomRight,
-                child: Stack(
-                  children: <Widget>[
-                    Image(
-                      image: AssetImage('assets/HomeView.jpg'),
-                      fit: BoxFit.fitWidth,
+            Stack(
+              children: <Widget>[
+                Positioned(
+                  top: topimg,
+                  right: 0,
+                  left: 0,
+                  child: Image(
+                    image: AssetImage('assets/HomeView.jpg'),
+                    fit: BoxFit.fitWidth,
+                  ),
+                ),
+                Positioned(
+                  top: toptext,
+                  left: 50,
+                  child: Container(
+                    width: MediaQuery.of(context).size.width / 2,
+                    alignment: Alignment.topLeft,
+                    child: Column(
+                      children: [
+                        Align(
+                          alignment: Alignment.center,
+                          child: Text('Hello, I\u0027m\nGaurav Yadav',
+                              style: GoogleFonts.montserrat(
+                                textStyle: TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 60,
+                                    fontWeight: FontWeight.w400),
+                              )),
+                        ),
+                        SizedBox(
+                          height: 30,
+                        ),
+                        Text(
+                            'An Engineering Student and a tech enthusiast working \nto better understand the core concepts behind different \npopular Technologies like Artificial Intelligence,DevOps,\nCloud Computing etc.',
+                            style: GoogleFonts.montserrat(
+                              textStyle: TextStyle(
+                                color: Colors.grey[700],
+                              ),
+                            )),
+                        SizedBox(
+                          height: 10,
+                        ),
+                      ],
                     ),
+                  ),
+                ),
+                ListView(
+                  children: <Widget>[
                     Container(
-                      child: Column(
-                        children: <Widget>[
-                          SizedBox(
-                            height: 100,
-                          ),
-                          Column(
-                            children: [
-                              SizedBox(
-                                height: MediaQuery.of(context).size.height / 15,
-                              ),
-                              Row(
-                                children: [
-                                  SizedBox(width: 80),
-                                  Container(
-                                    width:
-                                        MediaQuery.of(context).size.width / 2,
-                                    alignment: Alignment.topLeft,
-                                    child: Column(
-                                      children: [
-                                        Align(
-                                          alignment: Alignment.center,
-                                          child: Text(
-                                              'Hello, I\u0027m\nGaurav Yadav',
-                                              style: GoogleFonts.montserrat(
-                                                textStyle: TextStyle(
-                                                    color: Colors.white,
-                                                    fontSize: 60,
-                                                    fontWeight:
-                                                        FontWeight.w400),
-                                              )),
-                                        ),
-                                        SizedBox(
-                                          height: 30,
-                                        ),
-                                        Text(
-                                            'An Engineering Student and a tech enthusiast working \nto better understand the core concepts behind different \npopular Technologies like Artificial Intelligence,DevOps,\nCloud Computing etc.',
-                                            style: GoogleFonts.montserrat(
-                                              textStyle: TextStyle(
-                                                color: Colors.grey[700],
-                                              ),
-                                            )),
-                                        SizedBox(
-                                          height: 10,
-                                        ),
-                                      ],
-                                    ),
-                                  ),
-                                ],
-                              ),
-                            ],
-                          )
-                        ],
-                      ),
+                      height: 2700,
+                      color: Colors.transparent,
                     )
                   ],
-                ),
-              ),
+                )
+              ],
             ),
             Topbar(),
             SideBar(),
           ],
         ),
       ),
-    );
+    ));
   }
 }
--- lib/Screens/HomePage/HomePage.dart
+++ lib/Screens/HomePage/HomePage.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-
 import 'DesktopHomePage.dart';
 import 'MobileHomePage.dart';
 
