907a3f77caebde381ccc5cf0c24bacd4bf66f2de
gauravmehta13 <270mehta@gmail.com>
2021-01-18 14:51:48 +0530

    minor chnges

--- lib/Screens/Projects/apps.dart
+++ lib/Screens/Projects/apps.dart
@@ -1,38 +1,98 @@
 import 'package:MyPortfolio/Topbar.dart';
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class Applications extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Column(
-      children: [
-        Topbar(),
-        CarouselSlider(
-          items: [
-            RaisedButton(
-              onPressed: () {},
-              materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
-              padding: EdgeInsets.all(0),
-              child: Container(
-                decoration: BoxDecoration(
-                  image: DecorationImage(
-                      image: NetworkImage(
-                          'https://raw.githubusercontent.com/gauravmehta13/Portfolio/master/Assets/RECON.png'),
-                      fit: BoxFit.cover),
-                  borderRadius: BorderRadius.all(Radius.circular(12)),
-                ),
-              ),
-            )
-          ],
-          options: CarouselOptions(
-              height: MediaQuery.of(context).size.height - 100,
-              viewportFraction: 0.8,
-              autoPlay: true,
-              enlargeCenterPage: true),
+        appBar: AppBar(
+          backgroundColor: Colors.transparent,
+          elevation: 0,
+          title: Text(
+            'Applications',
+            style: GoogleFonts.montserrat(
+              textStyle: TextStyle(fontSize: 30, fontWeight: FontWeight.w600),
+            ),
+          ),
+          centerTitle: true,
         ),
-      ],
-    ));
+        body: Column(
+          children: [
+            SizedBox(
+              height: 20,
+            ),
+            CarouselSlider(
+              items: [
+                RaisedButton(
+                  color: Colors.transparent,
+                  onPressed: () {},
+                  materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
+                  padding: EdgeInsets.all(0),
+                  child: Container(
+                    decoration: BoxDecoration(
+                      image: DecorationImage(
+                          image: NetworkImage(
+                              'https://raw.githubusercontent.com/gauravmehta13/Portfolio/master/Assets/ReCon.%20(2).png'),
+                          fit: BoxFit.cover),
+                      borderRadius: BorderRadius.all(Radius.circular(12)),
+                    ),
+                  ),
+                ),
+                RaisedButton(
+                  color: Colors.transparent,
+                  onPressed: () {},
+                  materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
+                  padding: EdgeInsets.all(0),
+                  child: Container(
+                    decoration: BoxDecoration(
+                      image: DecorationImage(
+                          image: NetworkImage(
+                              'https://raw.githubusercontent.com/gauravmehta13/Portfolio/master/Assets/RVIVE.png'),
+                          fit: BoxFit.cover),
+                      borderRadius: BorderRadius.all(Radius.circular(12)),
+                    ),
+                  ),
+                ),
+                RaisedButton(
+                  color: Colors.transparent,
+                  onPressed: () {},
+                  materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
+                  padding: EdgeInsets.all(0),
+                  child: Container(
+                    decoration: BoxDecoration(
+                      image: DecorationImage(
+                          image: NetworkImage(
+                              'https://raw.githubusercontent.com/gauravmehta13/Portfolio/master/Assets/WA.png'),
+                          fit: BoxFit.cover),
+                      borderRadius: BorderRadius.all(Radius.circular(12)),
+                    ),
+                  ),
+                ),
+                RaisedButton(
+                  color: Colors.transparent,
+                  onPressed: () {},
+                  materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
+                  padding: EdgeInsets.all(0),
+                  child: Container(
+                    decoration: BoxDecoration(
+                      image: DecorationImage(
+                          image: NetworkImage(
+                              'https://raw.githubusercontent.com/gauravmehta13/Portfolio/master/Assets/API.png'),
+                          fit: BoxFit.cover),
+                      borderRadius: BorderRadius.all(Radius.circular(12)),
+                    ),
+                  ),
+                )
+              ],
+              options: CarouselOptions(
+                  height: MediaQuery.of(context).size.height - 100,
+                  viewportFraction: 0.7,
+                  autoPlay: true,
+                  enlargeCenterPage: true),
+            ),
+          ],
+        ));
   }
 }
