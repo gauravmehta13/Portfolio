3546a28c00845e12d9d30380ac9e4afb4215105b
gauravmehta13 <270mehta@gmail.com>
2021-01-18 14:41:32 +0530

    minor

--- lib/Screens/Projects/apps.dart
+++ lib/Screens/Projects/apps.dart
@@ -1,3 +1,4 @@
+import 'package:MyPortfolio/Topbar.dart';
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 
@@ -5,16 +6,33 @@ class Applications extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: CarouselSlider(
-      items: [
-        Container(
-          decoration: BoxDecoration(
-            color: Colors.blue,
-            borderRadius: BorderRadius.all(Radius.circular(12)),
-          ),
-        )
+        body: Column(
+      children: [
+        Topbar(),
+        CarouselSlider(
+          items: [
+            RaisedButton(
+              onPressed: () {},
+              materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
+              padding: EdgeInsets.all(0),
+              child: Container(
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                      image: NetworkImage(
+                          'https://raw.githubusercontent.com/gauravmehta13/Portfolio/master/Assets/RECON.png'),
+                      fit: BoxFit.cover),
+                  borderRadius: BorderRadius.all(Radius.circular(12)),
+                ),
+              ),
+            )
+          ],
+          options: CarouselOptions(
+              height: MediaQuery.of(context).size.height - 100,
+              viewportFraction: 0.8,
+              autoPlay: true,
+              enlargeCenterPage: true),
+        ),
       ],
-      options: CarouselOptions(height: MediaQuery.of(context).size.height),
     ));
   }
 }
